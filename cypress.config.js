20891918c937a15c3fa6f5e503152f189250ead3
Chrystianne <chrystiannem@gmail.com>
2025-10-31 13:57:24 -0300

    Alterações

--- cypress.config.js
+++ cypress.config.js
@@ -9,6 +9,12 @@ module.exports = defineConfig({
     json: true
   },
   e2e: {
+    // 🔧 Aumenta o tempo limite de carregamento da página
+    pageLoadTimeout: 120000, // 2 minutos
+
+    // ⏱️ Aumenta o tempo padrão de comandos (clicar, digitar, etc.)
+    defaultCommandTimeout: 10000, // 10 segundos
+
     setupNodeEvents(on, config) {
       require('cypress-mochawesome-reporter/plugin')(on);
     },
